0ed840373bb3c806658e83d3a36a3e285a117d40
Amy Boyer <amy@zooniverse.org>
2017-01-25 21:28:25 -0600

    add a few more command-line parameters and define a main function instead of just putting it in the module

--- simple.py
+++ simple.py
@@ -23,6 +23,8 @@ INPUT_FILE = LAND_MASK
 GENERATE_MASK_TILES = False
 REJECT_TILES = False
 VISUALIZE_SORT = False
+REMOVE_LAND = False
+REMOVE_CLOUDS = False
 
 retained_tiles = []
 no_water = []
@@ -31,8 +33,11 @@ too_cloudy = []
 def usage():
     print("USAGE")
     print("To regenerate mask tiles, use --generate-mask")
-    print("To show which tiles would be rejected, use --visualize")
+    print("To reject tiles that are only land, use --remove-land")
+    print("To reject tiles that are too cloudy, use --remove-clouds")
+    print("To reject both land and cloud tiles, use --clean")
     print("To sort tiles into reject and accept, use --reject")
+    print("To show which tiles would be rejected, use --visualize")
 
 def generate_tiles():
     print("Generating mask tiles of "+str(GRID_SIZE)+"x"+str(GRID_SIZE)+" pixels")
@@ -82,49 +87,71 @@ def remove_clouds(tiles, too_cloudy):
 
     return accum
 
+def parse_options():
+    global GENERATE_MASK_TILES, VISUALIZE_SORT, REJECT_TILES, REMOVE_LAND, REMOVE_CLOUDS
+    for arg in argv:
+        if(arg=="--generate-mask"):
+            GENERATE_MASK_TILES = True
+        if(arg=="--visualize"):
+            VISUALIZE_SORT = True
+        if(arg=="--reject"):
+            REJECT_TILES = True
+        if(arg=="--clean"):
+            REMOVE_LAND = True
+            REMOVE_CLOUDS = True
+        if(arg=="--remove-land"):
+            REMOVE_LAND = True
+        if(arg=="--remove-clouds"):
+            REMOVE_CLOUDS = True
 
-for arg in argv:
-    if(arg=="--generate-mask"):
-        GENERATE_MASK_TILES = True
-    if(arg=="--visualize"):
-        VISUALIZE_SORT = True
-    if(arg=="--reject"):
-        REJECT_TILES = True
+def main():
+    global retained_tiles, no_water, too_cloudy
 
-if(not GENERATE_MASK_TILES and not REJECT_TILES and not VISUALIZE_SORT):
-    usage()
-else:
-    print("Processing scene " + SCENE)
+    parse_options()
 
-if(GENERATE_MASK_TILES):
-    print("Generating tiles")
-    build_scratch(SCRATCH_PATH)
-    generate_tiles()
-else:
-    print("Skipping mask tile generation")
+    if(not GENERATE_MASK_TILES and not REJECT_TILES and not VISUALIZE_SORT):
+        usage()
+    else:
+        print("Processing scene " + SCENE)
 
-if(REJECT_TILES or VISUALIZE_SORT):
+    if(GENERATE_MASK_TILES):
+        print("Generating tiles")
+        build_scratch(SCRATCH_PATH)
+        generate_tiles()
+    else:
+        print("Skipping mask tile generation")
 
-    retained_tiles = get_files_by_extension(path.join(SCRATCH_PATH, "land"), "png")
+    if(REJECT_TILES or VISUALIZE_SORT):
 
-    print("Examining " + str(len(retained_tiles)) + " tiles for water")
-    result = remove_land(retained_tiles, no_water)
-    print("Found " + str(len(result)) + " tiles with water or clouds to be retained (" + str(len(no_water)) + " tiles rejected)")
-    retained_tiles = result
+        retained_tiles = get_files_by_extension(path.join(SCRATCH_PATH, "land"), "png")
 
-    print("Examining " + str(len(retained_tiles)) + " tiles for clouds")
-    result = remove_clouds(retained_tiles, too_cloudy)
-    print("Found " + str(len(result)) + " tiles with few enough clouds to be retained (" + str(len(too_cloudy)) + " tiles rejected)")
-    retained_tiles = result
+        if(REMOVE_LAND):
+            print("Examining " + str(len(retained_tiles)) + " tiles for water")
+            result = remove_land(retained_tiles, no_water)
+            print("Found " + str(len(result)) + " tiles with water or clouds to be retained (" + str(len(no_water)) + " tiles rejected)")
+            retained_tiles = result
+        else:
+            print("Skipping land removal")
 
-if(VISUALIZE_SORT):
+        if(REMOVE_CLOUDS):
+            print("Examining " + str(len(retained_tiles)) + " tiles for clouds")
+            result = remove_clouds(retained_tiles, too_cloudy)
+            print("Found " + str(len(result)) + " tiles with few enough clouds to be retained (" + str(len(too_cloudy)) + " tiles rejected)")
+            retained_tiles = result
+        else:
+            print("Skipping cloud removal")
 
-    print("Generating visualization of retained tiles")
+    if(VISUALIZE_SORT):
 
-    width = img.get_width(INPUT_FILE)
+        print("Generating visualization of retained tiles")
 
-    land = img.generate_rectangles(no_water, width, GRID_SIZE)
-    clouds = img.generate_rectangles(too_cloudy, width, GRID_SIZE)
-    water = img.generate_rectangles(retained_tiles, width, GRID_SIZE)
+        width = img.get_width(INPUT_FILE)
 
-    img.draw_visualization(land, clouds, water, INPUT_FILE)
+        land = img.generate_rectangles(no_water, width, GRID_SIZE)
+        clouds = img.generate_rectangles(too_cloudy, width, GRID_SIZE)
+        water = img.generate_rectangles(retained_tiles, width, GRID_SIZE)
+
+        img.draw_visualization(land, clouds, water, INPUT_FILE)
+
+if __name__ == "__main__":
+    main()
