88755299483c074e0e85d8321b5861b0cbcdcb8e
Amy Boyer <amy@zooniverse.org>
2017-06-23 10:50:48 -0500

    update generate_all script to use new calling convention

--- generate_all.sh
+++ generate_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find temp/L* -type d | xargs basename | xargs -P 4 -I {} python simple.py --full {}
+find temp/L* -type d | xargs -P 4 -I {} python simple.py --full {}
 echo "Mogrifying, please wait"
 find L*/accepted/*.png | xargs -P 4 mogrify -resize 500x500
 echo "Done"
