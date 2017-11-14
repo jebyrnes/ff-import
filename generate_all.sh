08ce2594d0030d25c1f4b6a9f2804d7660b2496c
Amy Boyer <amy@zooniverse.org>
2017-11-14 16:02:30 -0600

    hopefully keep the find command from blowing up with too many results

--- generate_all.sh
+++ generate_all.sh
@@ -2,5 +2,5 @@
 
 find temp/L* -type d | xargs -P 4 -I {} python simple.py --full {}
 echo "Mogrifying, please wait"
-find L*/accepted/*.png | xargs -P 4 mogrify -resize 500x500
+find L*/accepted | xargs -P 2 -I {} find {}/*.png | xargs - P 2 mogrify -resize 500x500
 echo "Done"
