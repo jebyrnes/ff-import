a70bc6314a4d87e716c047ebd8c5bd0435e5ec34
Amy Boyer <amy@zooniverse.org>
2017-11-15 11:06:52 -0600

    oops there was a problem with the mogrify command

--- generate_all.sh
+++ generate_all.sh
@@ -2,5 +2,5 @@
 
 find temp/L* -type d | xargs -P 4 -I {} python simple.py --full {}
 echo "Mogrifying, please wait"
-find L*/accepted | xargs -P 2 -I {} find {}/*.png | xargs - P 2 mogrify -resize 500x500
+find L*/accepted -maxdepth 0 | xargs -I {} -P 4 mogrify -resize 500x500 {}/*.png
 echo "Done"
