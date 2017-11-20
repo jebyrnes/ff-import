2ebf96ea618568c051ca3b50a35f0456ef843040
Amy Boyer <amy@zooniverse.org>
2017-11-20 10:47:19 -0600

    tell imagemagick not to get too clever for its own good

--- generate_all.sh
+++ generate_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export MAGICK_THREAD_LIMIT=1
 find temp/L* -type d | xargs -P 4 -I {} python simple.py --full {}
 echo "Mogrifying, please wait"
 find L*/accepted -maxdepth 0 | xargs -I {} -P 4 mogrify -resize 500x500 {}/*.png
